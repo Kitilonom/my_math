a4740507210b05ff03fb032413040a3bd249e25d
Сова <sova@MacBook-Air-Administrator.local>
2022-08-31 22:25:11 +0300

    add gitignore, .h, abs

---
+++ src/my_abs.c
@@ -0,0 +1,6 @@
+#include "my_math.h"
+
+int my_abs(int x) {
+    if (x < 0) x = -x;
+    return x;
+}
---
+++ src/my_math.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define my_PI 3.14159265358979323846
+#define my_e 2.71828182845904523536
+
+int my_abs(int x);
+long double my_acos(double x);
+long double my_asin(double x);
+long double my_atan(double x);
+long double my_ceil(double x);
+long double my_cos(double x);
+long double my_exp(double x);
+long double my_fabs(double x);
+long double my_floor(double x);
+long double my_fmod(double x, double y);
+long double my_log(double x);
+long double my_pow(double base, double exp);
+long double my_sin(double x);
+long double my_sqrt(double x);
+long double my_tan(double x);
